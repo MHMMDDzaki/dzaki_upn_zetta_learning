45a762d5ed522a306933149d68367920d0fb6ceb
M.Dzaki <dzakibs@gmail.com>
2022-10-25 14:02:42 +0700

    js day2

--- day1.js
+++ day1.js
@@ -1,43 +1,46 @@
-// const book = [
-//     {name:"Sword Art Online",price:120000,printing_status:true},
-//     {name:"Harry Potter",price:140000,printing_status:true},
-//     {name:"Uncharted",price:110000,printing_status:false}
-// ]
+const book = [
+    { name: "Sword Art Online", price: 120000, printing_status: true, stock: 10, purchased: 5 },
+    { name: "Harry Potter", price: 140000, printing_status: true, stock: 10, purchased: 5 },
+    { name: "Uncharted", price: 110000, printing_status: false, stock: 0, purchased: 0 },
+    { name: "Mushoku Tensei", price: 110000, printing_status: true, stock: 0, purchased: 10 }
+]
 
-// function purchaseBook(name){
-//     let status = ""
-//     for(let i = 0; i<book.length; i++){
-//         if(book[i].name == name) {
-//             status = "ada"
-//             break
-//         } else {
-//             status = "tidak ada"
-//         }
-//     }
-//     return console.log(status)
-// }
-
-// purchaseBook("Harry Potter")
+let status = ""
 let tax = 10
 let discount = 20
 
-function purchaseBook(param1,param2,param3){
-    let title = param1
-    let price = param2
-    let print_status = param3
-
-    const totalDiscount = price * (discount/100)
-    const priceAfterDiscount = price - totalDiscount
-    const totalTax = priceAfterDiscount * (tax/100)
-    const priceAfterTax = priceAfterDiscount - totalTax
-
-    console.group()
-    console.log("Original Price ", price)
-    console.log("Discount Amount ", totalDiscount)
-    console.log("Price after discount ", priceAfterDiscount)
-    console.log("tax amount ", totalTax)
-    console.log("Price after tax ", priceAfterTax)
-    console.groupEnd()
+function purchaseBook(name,purchasing) {
+    for (let i = 0; i < book.length; i++) {
+        if (book[i].name === name && book[i].printing_status != false) {
+            if (book[i].stock == 0) {
+                status = "stok buku habis"
+                break
+            } else {
+                const totalDiscount = book[i].price * (discount / 100)
+                const priceAfterDiscount = book[i].price - totalDiscount
+                const totalTax = priceAfterDiscount * (tax / 100)
+                const priceAfterTax = priceAfterDiscount - totalTax
+                console.group()
+                console.log("Original Price ", book[i].price)
+                console.log("Discount Amount ", totalDiscount)
+                console.log("Price after discount ", priceAfterDiscount)
+                console.log("tax amount ", totalTax)
+                console.log("Price after tax ", priceAfterTax)
+                console.log("Stock ", book[i].stock)
+                if(book[i].stock >= purchasing) {
+                    console.log("Book can be repurchased")
+                } else {
+                    console.log("Book cannot repurchased")
+                }
+                console.groupEnd()
+                break
+            }
+        } else {
+            status = "buku tidak tersedia"
+        }
+    }
+    console.log(status)
 }
 
-purchaseBook("Harry Potter",120000,true)
+// masukkan judul dan stok yang ingin dipesan
+purchaseBook("Sword Art Online",11)
