85ebd3da6bd9a9c9261a50ad530682c3f36c08c8
M.Dzaki <dzakibs@gmail.com>
2022-10-27 20:22:12 +0700

    update typescript day 2

--- tscDay2.js
+++ tscDay2.js
@@ -1,7 +1,10 @@
+var result1;
 function random2(a) {
-    return console.log(a.join(' '));
+    result1 = a.join(" ");
+    console.log(result1);
+    return result1;
 }
-random2(['1', 'data', '3', 'result']);
+random2([1, 'data', '3', 'result', true]);
 random2(['Bejo', 'has', '4', 'sport', 'car']);
 function random3(pt) {
     return console.log(pt);
--- tscDay2.ts
+++ tscDay2.ts
@@ -1,9 +1,11 @@
-function random2(a: (string | number)[]){
-    return console.log(a.join(' '))
-
+let result1: string
+function random2(a: (string | number | boolean)[]): string{
+    result1 = a.join(" ")
+    console.log(result1)
+    return result1
 }
 
-random2(['1','data','3','result'])
+random2([1,'data','3','result',true])
 random2(['Bejo','has','4','sport','car'])
 
 // When to use array,tuple,and object ?
