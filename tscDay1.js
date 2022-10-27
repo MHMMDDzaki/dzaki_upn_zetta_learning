460974aa7de455178abbbc84a8a497ea9f31ee52
M.Dzaki <dzakibs@gmail.com>
2022-10-27 15:25:44 +0700

    typescript day1

---
+++ tscDay1.js
@@ -0,0 +1,4 @@
+function random(a, b, c) {
+    return console.log(a.slice(c, b));
+}
+random('Learning Typescript is different than Javascript', 19, 9);
---
+++ tscDay1.ts
@@ -0,0 +1,5 @@
+function random(a: string,b: number, c:number) {
+    return console.log(a.slice(c,b))
+}
+
+random('Learning Typescript is different than Javascript',19,9)
