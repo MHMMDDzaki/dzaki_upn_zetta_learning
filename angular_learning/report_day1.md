a0bb80cf92395e794afbfb5031595f0e875e085f
M.Dzaki <dzakibs@gmail.com>
2022-10-30 14:19:01 +0700

    angular day1 1.0

---
+++ angular_learning/report_day1.md
@@ -0,0 +1,9 @@
+1. Angular merupakan framework untuk membuat aplikasi dinamis tanpa perlu merefresh browseryang berbasis html dan typescript. 
+   Konsep dari angular sendiri merupakan modul yang terorganisir dari beberapa komponen. Modul sendiri adalah kumpulan kode
+   yang memiliki beberapa fungsi sendiri. komponen merupakan kode yang biasanya memiliki 1 fungsi saja.
+
+
+QUIZ
+1.  Saya akan mengubah page youtube menjadi beberapa komponen seperti komponen sidebar yang berisi list menu, komponen header yang berisi logo, kotak pencarian, dan logo akun , dan komponen konten utama yang berisi list video
+2.  Untuk menghubungkan komponen pada modul modul yang terpisah bisa menggunakan router
+3.  
---
+++ angular_learning/src/app/module2/module2.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
+import { Component5Component } from './component5/component5.component';
+import { Component6Component } from './component6/component6.component';
+import { Component7Component } from './component7/component7.component';
+
+
+
+@NgModule({
+  declarations: [
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component,
+    Component5Component,
+    Component6Component,
+    Component7Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Module2Module { }
