079735347169277d469e34cfef400d65188015aa
M.Dzaki <dzakibs@gmail.com>
2022-11-01 16:08:09 +0700

    Revert "angular day3 kasaran"
    This reverts commit ecffe4d8d084d7c85ab1c901ab26629332ea2475.

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
-import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CartComponent
+    CardComponent
   ],
   imports: [
     BrowserModule,
