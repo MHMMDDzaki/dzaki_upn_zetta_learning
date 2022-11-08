f4dd4894994bcbcd9ba4db1b16284f26b0bfab30
M.Dzaki <dzakibs@gmail.com>
2022-11-08 20:59:08 +0700

    angular day 5

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialExampleModule } from '../material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatNativeDateModule } from '@angular/material/core'
@@ -19,7 +20,8 @@ import { CartComponent } from './cart/cart.component'
     FormsModule,
     BrowserAnimationsModule,
     MatNativeDateModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MaterialExampleModule
 
   ],
   providers: [],
