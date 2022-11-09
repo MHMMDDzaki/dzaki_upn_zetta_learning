ef3c30555de111a57bb3efa6e4df48dcb759a139
M.Dzaki <dzakibs@gmail.com>
2022-11-09 11:01:02 +0700

    angular_day6

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialExampleModule } from '../material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatNativeDateModule } from '@angular/material/core'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +13,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
