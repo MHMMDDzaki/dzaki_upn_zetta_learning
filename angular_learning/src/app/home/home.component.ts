c4938b9be7e77881982dcb97a45eb2dd47e05585
M.Dzaki <dzakibs@gmail.com>
2022-11-11 15:29:46 +0700

    angular day 7

--- angular_learning/src/app/home/home.component.ts
+++ angular_learning/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserFormService } from '../user-form.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  listUser = []
+  constructor(private userFormService: UserFormService, private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
 }
